d87c7c070e27c8fd6a512a457eabed19a24a9d18
said <said.dahir.said@gmail.com>
2021-11-30 23:51:12 -0800

    added frontend

--- src/ravensports/urls.py
+++ src/ravensports/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
+from rest_framework import routers                 
+from sports import views
+import sports        
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('sports.urls'))
 ]
