79f96b254250ccfc5fc4ff1fc1d353cd2cb9da1d
nchowdri <nchowdri@insulet.com>
2022-02-12 17:51:31 -0800

    added new model for player per game stats and updated html

--- src/sports/models/__init__.py
+++ src/sports/models/__init__.py
@@ -2,10 +2,10 @@
 import imp
 from .team import Team
 from .standings import Standings
-#from .player import Player
+from .player_per_game_stats import PlayerPerGameStats
 from django.contrib import admin
 
 # TODO admin.site register all Models
 admin.site.register(Standings)  #test
 admin.site.register(Team)
-#admin.site.register(Player)
+admin.site.register(PlayerPerGameStats)
---
+++ src/sports/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+import json
+import datetime
+
+import datetime
+
+now = datetime.datetime.now()
+
+
+def home(request):
+    # api request
+    today_date = datetime.date.today()
+    url = "https://api-nba-v1.p.rapidapi.com/games/date/{}".format(today_date)
+
+    headers = {
+        'x-rapidapi-host': "api-nba-v1.p.rapidapi.com",
+        'x-rapidapi-key': "17c670de23mshfd6cbf40f8dcb0ap1d1114jsn76ac1068f254"
+    }
+
+    api_request = requests.get(url, headers=headers)
+
+    try:
+        api = json.loads(api_request.content)
+    except:
+        api = "Error..."
+
+    return render(request, 'index.html', {'api': api})
